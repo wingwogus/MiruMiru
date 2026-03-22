3bac660b489236dad6027bdbb46122f67bc34b5b
monikhyun <mih2001103@naver.com>
2026-03-22 09:13:27 +0900

    feat/ SecurityConfig 설정 수정 및 WebSocketConfig 추가

---
+++ backend/api/src/main/kotlin/com/example/config/WebSocketConfig.kt
@@ -0,0 +1,35 @@
+package com.example.config
+
+import com.example.api.websocket.WebSocketAuthChannelInterceptor
+import com.example.application.security.TokenProvider
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.ChannelRegistration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig(
+    private val tokenProvider: TokenProvider,
+) : WebSocketMessageBrokerConfigurer {
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry
+            .addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+            .withSockJS()
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.setApplicationDestinationPrefixes("/pub")
+        registry.enableSimpleBroker("/sub", "/queue")
+        registry.setUserDestinationPrefix("/user")
+    }
+
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        registration.interceptors(WebSocketAuthChannelInterceptor(tokenProvider))
+    }
+}
+

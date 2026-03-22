903ccd51378ad19981675cc7c7e52291f7d6d5ee
monikhyun <mih2001103@naver.com>
2026-03-22 09:16:41 +0900

    feat/ Board Repo 커밋

--- backend/domain/src/main/kotlin/com/example/domain/board/BoardRepository.kt
+++ backend/domain/src/main/kotlin/com/example/domain/board/BoardRepository.kt
@@ -1,8 +1,10 @@
 package com.example.domain.board
 
+import com.example.domain.university.University
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface BoardRepository : JpaRepository<Board, Long> {
+    fun findByUniversityAndName(university: University, name: String): Board?
     fun findAllByUniversityIdOrderByIdAsc(universityId: Long): List<Board>
     fun findByIdAndUniversityId(id: Long, universityId: Long): Board?
     fun findByUniversityIdAndCode(universityId: Long, code: String): Board?
